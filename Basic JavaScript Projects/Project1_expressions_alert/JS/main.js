d23ecdabfd9211d363f7d0f6fbbad11f0f89ecd6
Giorgina3 <115377895+Giorgina3@users.noreply.github.com>
2023-03-08 18:57:47 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+document.write('<br/>'); //Putin spaces between Dr. Seuss
+                         //and dad
+
+var Family = "The Smiths", Dad = "Jeremy", Mom = "Lisa",
+      Daughter = "Penny", Son = "Mike";
+document.write(Dad);
+document.write('<br/>');
+var Family = Family.fontcolor("red");   //Using the fontcolot method
+document.write(Family);
+document.write('<br/>'); 
+document.write(1+2) //Writing an expresion
